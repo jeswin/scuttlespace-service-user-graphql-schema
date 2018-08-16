52c014955d1a88b5d69d98e1971e752f39562930
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-16 10:54:46 +0530

    Fix npmlink

--- npmlink.sh
+++ npmlink.sh
@@ -1,2 +1,12 @@
-rm -rf ../scuttlespace-api-common/node_modules
-npm link ../scuttlespace-api-common
+set -e 
+BASHO_VERSION=$(basho -v | basho 'x.split(".")[2]' -j 'parseInt(x) >= 43 && "OK"')
+
+if [[ $BASHO_VERSION == "OK" ]]
+then
+  (
+    echo scuttlespace-service-common
+  ) | basho -n proj -e 'cd ../${x} && npm link && [ -f npmlink.sh ] && ./npmlink.sh || echo skipped' \
+        -s proj -j '`update ${x}`'
+else
+  echo "Install basho version 0.0.43 or higher."
+fi
